fce564d4be7000bf2739a2ff77e9c28ce8ec6d90
tuanna1408 <nguyenanhtuan14081995@gmail.com>
2016-02-22 01:18:32 +0700

    tuan commit

--- B_Sources/00_Android/EventPlus/app/src/main/java/com/poly/eventplus/activity/MainActivity.java
+++ B_Sources/00_Android/EventPlus/app/src/main/java/com/poly/eventplus/activity/MainActivity.java
@@ -1,9 +1,10 @@
-package com.poly.eventplus;
+package com.poly.eventplus.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.poly.eventplus.R;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
